d6b546bd7ff06be5949628ecd2ec22b0563bdc82
Mahmoud Hamed <dev.mahmoudhamed@gmail.com>
2022-02-04 13:02:23 +0200

    fix Program.cs

--- Program.cs
+++ Program.cs
@@ -1,5 +1,4 @@
-<<<<<<< HEAD
-﻿using System;
+using System;
 using Scraper;
 
 namespace test
@@ -18,25 +17,4 @@ namespace test
 
         }
     }
-=======
-﻿using System;
-using Scraper;
-
-namespace test
-{
-    class Program
-    {
-
-
-        static void Main(string[] args)
-        {
-            Console.WriteLine("What are you looking for ?");
-            string searchKeyword = Console.ReadLine();
-
-            // Zlibrary.startServer(searchKeyword);
-            // SpringerLink.startServer(searchKeyword);
-
-        }
-    }
->>>>>>> 03343751cdf5d97433b6051eae4c3805cacdedce
 }
